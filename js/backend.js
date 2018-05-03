4828f89a7747d887b88a593bd56209a74f03b5d0
turnt <frostmail@inbox.ru>
2018-05-03 22:11:25 +0300

    Пункт 1: создан backend.js с экспортируемыми функциями load и save

---
+++ js/backend.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function(){
+  var load = function (onLoad, onError) {
+
+  };
+
+  var save = function (data, onLoad, onError) {
+
+  };
+
+  window.backend = {
+    load: load,
+    save: save,
+  }
+})();
